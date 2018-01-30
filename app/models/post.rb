9846810ddd7afb8ac43d67d0a9d0c08f58ade44a
Hoon Lee <yl.hoony@gmail.com>
2018-01-30 19:02:48 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  # before_save :make_title_case
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
 
   private
 
